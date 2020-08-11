f04bfa7565847797773be512bac37c26f6a45f85
chellakumars <33822021+chellakumars@users.noreply.github.com>
2020-08-11 20:47:05 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash/
+aws cloudformation deploy --template-file ./templates/alb-external.yaml --stack-name ECS-LoadBalancer --parameter-overrides $(cat ./parameters/us-west-2/alb-external.ini)"
+aws cloudformation deploy --template-file ./templates/ecs-service.yaml --stack-name Customer3-service --parameter-overrides $(cat ./parameters/us-west-2/customer1.ini)"
